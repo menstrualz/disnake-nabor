283e22330061b1ab2c72f878cbb8a2e2cceb30fd
menstrualz <127664990+menstrualz@users.noreply.github.com>
2024-02-23 17:46:54 +0200

    Update listeners.py

--- system/cogs/Events/listeners.py
+++ system/cogs/Events/listeners.py
@@ -1,29 +1,21 @@
-import asyncio
-import disnake
 from disnake.ext import commands
-from assets.databases import Database
-from assets.pool import db_pool
 
 
 class Listeners(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = Database()
 
     @commands.Cog.listener()
     async def on_connect(self):
-        await db_pool.create_pool()
-        await asyncio.sleep(1)
+        pass
 
     @commands.Cog.listener()
     async def on_disconnect(self):
-        await db_pool.close_pool()
-        await asyncio.sleep(1)
+        pass
 
     @commands.Cog.listener()
     async def on_resumed(self):
-        await db_pool.create_pool()
-        await asyncio.sleep(1)
+        pass
     
 def setup(bot):
     bot.add_cog(Listeners(bot))
