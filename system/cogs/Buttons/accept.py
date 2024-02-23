85b21feacd35d68451a5abbee5eec0053db5797e
menstrualz <127664990+menstrualz@users.noreply.github.com>
2024-02-23 18:02:19 +0200

    fix accept button

--- system/cogs/Buttons/accept.py
+++ system/cogs/Buttons/accept.py
@@ -11,19 +11,24 @@ class AcceptButton(disnake.ui.Button):
 
     async def callback(self, interaction: disnake.MessageInteraction) -> None:
         user = await interaction.guild.fetch_member(self.user)
-        embed = disnake.Embed(color=Color.GRAY)
-        embed.title = "Вам отказано в заявке!"
-        embed.description = f"{user.mention}, Ваша заявка была **принята** {interaction.author.mention}!\n" \
-                             "Удачи в **работе**, пупс!"
-        embed.set_thumbnail(url=user.display_avatar)
-        await user.send(embed=embed)
-        embed2 = disnake.Embed(color=Color.GRAY)
+        embed = disnake.Embed(
+            color=Color.GRAY, title="Ваша заявка принята!",
+            description=f"{user.mention}, Ваша заявка была **принята** {interaction.author.mention}!\n"
+                        "Удачи в **работе**, пупс!"
+        ).set_thumbnail(url=user.display_avatar)
+
+        try:
+            await user.send(embed=embed)
+        except disnake.Forbidden:
+            pass
+
         edited_embed = self.embed.copy()
-        edited_embed.set_footer(text=f"Рассматривает: {interaction.author.name}", icon_url=interaction.author.avatar)
+        edited_embed.set_footer(
+            text=f"Рассматривает: {interaction.author.display_name}",
+            icon_url=interaction.author.display_avatar
+        )
         await interaction.response.edit_message(embed=edited_embed, view=None)
         role = interaction.guild.get_role(RolesIds.AWAITING.value)
         if role in user.roles:
             await user.remove_roles(role)
-            await interaction.followup.send("Пользователь оповещен, не забудьте выдать роль!", ephemeral=True)
-        else:
-            return await interaction.followup.send("Пользователь оповещен, не забудьте выдать роль!", ephemeral=True)
+        await interaction.followup.send("Пользователь оповещен, не забудьте выдать роль!", ephemeral=True)
